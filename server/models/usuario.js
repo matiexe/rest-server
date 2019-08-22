59a676d609067eb4610d15e18d7ce70179cbbc81
matias Urra <matiexe31@gmail.com>
2019-08-22 20:03:16 -0300

    creacion de schema usuario, designacion de roles y validacion de usuarios, salvar documentos en la base de datos

---
+++ server/models/usuario.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+let Schema = mongoose.Schema;
+
+let rolesValidos = {
+    values:['ADMIN_ROLE','USER_ROLE'],
+    message:'{value} no es un rol valido'
+};
+
+let usuarioSchema = new Schema({
+    nombre:{
+        type: String,
+        required:[true,'El nombre es necesario']
+    },
+    email:{
+        type: String,
+        required:[true,'El email es necesario'],
+        unique:true
+    },
+    password:{
+        type: String,
+        required:[true,'El passwrod es necesario']
+    },
+    img:{
+        type: String,
+        required:false,
+    },
+    role:{
+        type:String,
+        default:'USER_ROLE',
+        enum: rolesValidos
+
+    },
+    estado:{
+        type:Boolean,
+        default:true
+    },
+    google:{
+        type:Boolean,
+        default:false
+    }
+});
+
+usuarioSchema.plugin(uniqueValidator,{message:'{PATH} debe de ser unico'})
+
+module.exports = mongoose.model('Usuario',usuarioSchema);
