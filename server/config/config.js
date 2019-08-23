2a167afee9602159fb987e433dabad72f9fcd38f
matias Urra <matiexe31@gmail.com>
2019-08-23 19:34:48 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV ||'dev';
 let urlDB;
-//if (process.env.NODE_ENV = 'dev'){
-  //  urlDB ='mongodb://localhost:27017/cafeteria';
-//}else{
-    urlDB ='mongodb+srv://admin:Qpal2190.@cluster0-dxljw.mongodb.net/cafe';
-//}
+if (process.env.NODE_ENV = 'dev'){
+   urlDB ='mongodb://localhost:27017/cafeteria';
+}else{
+    urlDB =process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
